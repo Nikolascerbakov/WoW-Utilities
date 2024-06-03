d80d75cac71139907a0abeba2d180ae409116fb5
Nikolascerbakov <scerbakovnikola@gmail.com>
2024-06-03 12:00:57 +0200

    added html structure

--- src/Tierlist.jsx
+++ src/Tierlist.jsx
@@ -2,8 +2,48 @@
 function Tierlist(){
 
     return(
-        <>
-        </>
+        <div className="container">
+            <div className="tierlist-container">
+                <div className="tierlist-tier">
+                    <div className="tierlist-grade">S</div>
+                    <ul className="tierlist-items">
+                        <a href="">
+                            <div className="tierlist-item-container">
+                                <div className="tierlist-item-image"></div>
+                                <div className="tierlist-item-label"><span>The Azure Vault</span></div>
+                            </div>
+                        </a>
+                    </ul>
+                </div>
+                <div className="tierlist-tier">
+                    <div className="tierlist-grade">A</div>
+                    <ul className="tierlist-items"></ul>
+                </div>
+                <div className="tierlist-tier">
+                    <div className="tierlist-grade">B</div>
+                    <ul className="tierlist-items"></ul>
+                </div>
+                <div className="tierlist-tier">
+                    <div className="tierlist-grade">C</div>
+                    <ul className="tierlist-items"></ul>
+                </div>
+                <div className="tierlist-tier">
+                    <div className="tierlist-grade">D</div>
+                    <ul className="tierlist-items"></ul>
+                </div>
+                <div className="tierlist-tier">
+                    <div className="tierlist-grade">E</div>
+                    <ul className="tierlist-items"></ul>
+                </div>
+                <div className="tierlist-tier">
+                    <div className="tierlist-grade">F</div>
+                    <ul className="tierlist-items"></ul>
+                </div>
+                
+
+            </div>
+
+        </div>
     )
 }
 
