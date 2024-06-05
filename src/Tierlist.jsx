625d2709b04edaaa02bd3db84efbee867c670a0f
Nikolascerbakov <scerbakovnikola@gmail.com>
2024-06-05 16:41:01 +0200

    added remaining colous and 2 more dungeons

--- src/Tierlist.jsx
+++ src/Tierlist.jsx
@@ -6,7 +6,7 @@ function Tierlist(){
             <div className="tierlist-container">
                 <div className="tierlist-tier">
                     <div className="tierlist-grade s-tier-bg">
-                        <h3 className="s-tier">S</h3>
+                        <h3 className="tier-letter-size">S</h3>
                     </div>
                         <ul className="tierlist-items">
                             <a href="https://www.wowhead.com/guide/dungeons/azure-vault-strategy">
@@ -20,25 +20,33 @@ function Tierlist(){
                         </ul>
                 </div>
                 <div className="tierlist-tier">
-                    <div className="tierlist-grade">A</div>
+                    <div className="tierlist-grade a-tier-bg">
+                        <h3 className="tier-letter-size">A</h3>
+                    </div>
                         <ul className="tierlist-items"> 
-                            <a href="">
-                                <div className="tierlist-item-container">
-                                    <div className="tierlist-item-image"></div>
-                                    <div className="tierlist-item-label"><span>The Nokhud Offensive</span></div>
+                            <a href="https://www.wowhead.com/guide/dungeons/nokhud-offensive-strategy">
+                                <div className="tierlist-item-container green-nokhud-bg green-nokhud-border-color">
+                                    <div className="tierlist-item-image">
+                                    <img src="src\assets\icons\dungeon_nokhud.jpg" className="icon-image-rounded"></img>
+                                    </div>
+                                    <div className="tierlist-item-label white-text"><span>The Nokhud Offensive</span></div>
                                 </div>
                             </a>
 
-                            <a href="">
-                                <div className="tierlist-item-container">
-                                    <div className="tierlist-item-image"></div>
-                                    <div className="tierlist-item-label"><span>Algethar Academy</span></div>
+                            <a href="https://www.wowhead.com/guide/dungeons/algethar-academy-strategy">
+                                <div className="tierlist-item-container cyan-academy-bg cyan-academy-border-color">
+                                    <div className="tierlist-item-image">
+                                    <img src="src\assets\icons\dungeon_dragonacademy.jpg" className="icon-image-rounded"></img>
+                                    </div>
+                                    <div className="tierlist-item-label white-text "><span>Algethar Academy</span></div>
                                 </div>
                             </a>
                         </ul>
                 </div>
                 <div className="tierlist-tier">
-                        <div className="tierlist-grade">B</div>
+                        <div className="tierlist-grade b-tier-bg">
+                            <h3 className="tier-letter-size">B</h3>
+                        </div>
                             <ul className="tierlist-items">
                                 <a href="">
                                     <div className="tierlist-item-container">
@@ -63,7 +71,9 @@ function Tierlist(){
                             </ul>
                 </div>
                 <div className="tierlist-tier">
-                    <div className="tierlist-grade">C</div>
+                    <div className="tierlist-grade c-tier-bg">
+                        <h3 className="tier-letter-size">C</h3>
+                    </div>
                         <ul className="tierlist-items">
                                 <a href="">
                                     <div className="tierlist-item-container">
@@ -82,15 +92,21 @@ function Tierlist(){
                         </ul>
                 </div>
                 <div className="tierlist-tier">
-                    <div className="tierlist-grade">D</div>
+                    <div className="tierlist-grade d-tier-bg">
+                        <h3 className="tier-letter-size">D</h3>
+                    </div>
                     <ul className="tierlist-items"></ul>
                 </div>
                 <div className="tierlist-tier">
-                    <div className="tierlist-grade">E</div>
+                    <div className="tierlist-grade e-tier-bg">
+                        <h3 className="tier-letter-size">E</h3>
+                    </div>
                     <ul className="tierlist-items"></ul>
                 </div>
                 <div className="tierlist-tier">
-                    <div className="tierlist-grade">F</div>
+                    <div className="tierlist-grade f-tier-bg">
+                        <h3 className="tier-letter-size">F</h3>
+                    </div>
                     <ul className="tierlist-items"></ul>
                 </div>
                 
