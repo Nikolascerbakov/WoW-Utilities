ae3eb66804c91f7b3a7f4576af28004d8c09b4c6
Nikolascerbakov <scerbakovnikola@gmail.com>
2024-06-05 16:04:23 +0200

    added tierlist css and first tier row

--- src/Tierlist.jsx
+++ src/Tierlist.jsx
@@ -5,27 +5,81 @@ function Tierlist(){
         <div className="container">
             <div className="tierlist-container">
                 <div className="tierlist-tier">
-                    <div className="tierlist-grade">S</div>
-                    <ul className="tierlist-items">
-                        <a href="">
-                            <div className="tierlist-item-container">
-                                <div className="tierlist-item-image"></div>
-                                <div className="tierlist-item-label"><span>The Azure Vault</span></div>
-                            </div>
-                        </a>
-                    </ul>
+                    <div className="tierlist-grade s-tier-bg">
+                        <h3 className="s-tier">S</h3>
+                    </div>
+                        <ul className="tierlist-items">
+                            <a href="https://www.wowhead.com/guide/dungeons/azure-vault-strategy">
+                                <div className="tierlist-item-container purple-vault-bg">
+                                    <div className="tierlist-item-image">
+                                        <img src="src\assets\icons\dungeon_azurevaults.jpg" className="icon-image-rounded"></img>
+                                    </div>
+                                    <div className="tierlist-item-label white-text">The Azure Vault</div>
+                                </div>
+                            </a>
+                        </ul>
                 </div>
                 <div className="tierlist-tier">
                     <div className="tierlist-grade">A</div>
-                    <ul className="tierlist-items"></ul>
+                        <ul className="tierlist-items"> 
+                            <a href="">
+                                <div className="tierlist-item-container">
+                                    <div className="tierlist-item-image"></div>
+                                    <div className="tierlist-item-label"><span>The Nokhud Offensive</span></div>
+                                </div>
+                            </a>
+
+                            <a href="">
+                                <div className="tierlist-item-container">
+                                    <div className="tierlist-item-image"></div>
+                                    <div className="tierlist-item-label"><span>Algethar Academy</span></div>
+                                </div>
+                            </a>
+                        </ul>
                 </div>
                 <div className="tierlist-tier">
-                    <div className="tierlist-grade">B</div>
-                    <ul className="tierlist-items"></ul>
+                        <div className="tierlist-grade">B</div>
+                            <ul className="tierlist-items">
+                                <a href="">
+                                    <div className="tierlist-item-container">
+                                        <div className="tierlist-item-image"></div>
+                                        <div className="tierlist-item-label"><span>Brackenhide Hollow</span></div>
+                                    </div>
+                                </a>
+
+                                <a href="">
+                                    <div className="tierlist-item-container">
+                                        <div className="tierlist-item-image"></div>
+                                        <div className="tierlist-item-label"><span>Ruby Life Pools</span></div>
+                                    </div>
+                                </a>
+
+                                <a href="">
+                                    <div className="tierlist-item-container">
+                                        <div className="tierlist-item-image"></div>
+                                        <div className="tierlist-item-label"><span>Halls of Infusion</span></div>
+                                    </div>
+                                </a>
+                            </ul>
                 </div>
                 <div className="tierlist-tier">
                     <div className="tierlist-grade">C</div>
-                    <ul className="tierlist-items"></ul>
+                        <ul className="tierlist-items">
+                                <a href="">
+                                    <div className="tierlist-item-container">
+                                        <div className="tierlist-item-image"></div>
+                                        <div className="tierlist-item-label"><span>Uldaman</span></div>
+                                    </div>
+                                </a>
+
+                                <a href="">
+                                    <div className="tierlist-item-container">
+                                        <div className="tierlist-item-image"></div>
+                                        <div className="tierlist-item-label"><span>Neltharus</span></div>
+                                    </div>
+                                </a>
+
+                        </ul>
                 </div>
                 <div className="tierlist-tier">
                     <div className="tierlist-grade">D</div>
