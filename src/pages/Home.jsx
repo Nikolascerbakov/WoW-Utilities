be0ebf261f6cc56d70c01376919e08693b0f63b3
Nikolascerbakov <scerbakovnikola@gmail.com>
2024-06-07 15:21:15 +0200

    added routing to m+ page from feature

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Link, useMatch, useResolvedPath } from 'react-router-dom'
 
 function Home(){
 
@@ -8,7 +9,10 @@ function Home(){
         <div className="container">
             <div className="container-features">
                 <div className="feature">
-                    <img src="src\assets\images\m+_tierlist_thumbnail.png" alt="m+ feature" />
+                <CustomLink to="/MythicPlus">
+                    <img className='feature-image' src="src\assets\images\m+_tierlist_thumbnail.png" alt="m+ feature" />
+                </CustomLink>
+                    
                 </div>
                 
             </div>
@@ -17,6 +21,16 @@ function Home(){
         </>
     
             )
+}
+
+function CustomLink({ to, children, ...props}){
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({path: resolvedPath.pathname, end: true})
+    return (
+        <li className={isActive ? "active" : ""}>
+            <Link to={to} {...props}>{children}</Link>
+        </li> 
+    )
 }
 
 export default Home
