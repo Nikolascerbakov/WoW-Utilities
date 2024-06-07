9b2ba2678986f680c8c055ce3cf4d2f6ddd2dbb9
Nikolascerbakov <scerbakovnikola@gmail.com>
2024-06-07 15:03:59 +0200

    created homepage branch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
 
 function Home(){
 
-    return (<h1>Home page</h1>)
+    return  (
+        <>
+        <h1>Home page</h1>
+        <h2>Current working features:</h2>
+        <div className="container">
+            <div className="container-features">
+                <img src="" alt="" />
+            </div>
+
+        </div>
+        </>
+    
+            )
 }
 
 export default Home
