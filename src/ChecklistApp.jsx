cff83f8837aa08aa0433bdf5b43754f06af08887
Nikolascerbakov <scerbakovnikola@gmail.com>
2024-06-20 16:06:15 +0200

    added checkboxes

--- src/ChecklistApp.jsx
+++ src/ChecklistApp.jsx
@@ -1,4 +1,5 @@
 
+
 function ChecklistApp(){
 
     return(
@@ -9,15 +10,33 @@ function ChecklistApp(){
                             <div className="grid-raid-slot">Raid slot</div>
                             <div className="grid-mythic-slot">MythicPlus slot</div>
                             <div className="grid-pvp-slot">PvP slot</div>
-                            <div className="grid-check1">Check1</div>
-                            <div className="grid-check2">Check2</div>
-                            <div className="grid-check3">Check3</div>
-                            <div className="grid-check4">Check4</div>
-                            <div className="grid-check5">Check5</div>
-                            <div className="grid-check6">Check6</div>
-                            <div className="grid-check7">Check7</div>
-                            <div className="grid-check8">Check8</div>
-                            <div className="grid-check9">Check9</div>
+                            <div className="grid-check1">
+                                <input type="checkbox"></input>
+                            </div>
+                            <div className="grid-check2">
+                                <input type="checkbox"></input>
+                            </div>
+                            <div className="grid-check3">
+                                <input type="checkbox"></input>
+                            </div>
+                            <div className="grid-check4">
+                                <input type="checkbox"></input>
+                            </div>
+                            <div className="grid-check5">
+                                <input type="checkbox"></input>
+                            </div>
+                            <div className="grid-check6">
+                                <input type="checkbox"></input>
+                            </div>
+                            <div className="grid-check7">
+                                <input type="checkbox"></input>
+                            </div>
+                            <div className="grid-check8">
+                                <input type="checkbox"></input>
+                            </div>
+                            <div className="grid-check9">
+                                <input type="checkbox"></input>
+                            </div>
                         </div>
         </>
     )
